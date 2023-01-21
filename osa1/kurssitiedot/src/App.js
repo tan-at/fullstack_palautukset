c04fb0731a2bd1564c7fa219ae236546ca0aa0a9
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2023-01-21 19:26:26 +0200

    1.2 Done

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -11,9 +11,9 @@ const Content = (props) => {
   console.log(props);
   return (
     <div>
-      <p>
-        {props.part} {props.exercises}{" "}
-      </p>
+      <Part osa={props.part1} tehtavat={props.exercises1} />
+      <Part osa={props.part2} tehtavat={props.exercises2} />
+      <Part osa={props.part3} tehtavat={props.exercises3} />
     </div>
   );
 };
@@ -23,6 +23,17 @@ const Total = (props) => {
   return (
     <div>
       <p>Number of exercises {props.total}</p>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        {props.osa} {props.tehtavat}
+      </p>
     </div>
   );
 };
@@ -39,9 +50,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
