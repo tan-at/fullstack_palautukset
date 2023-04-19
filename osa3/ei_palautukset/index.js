1b62c60a6a0aaa5b914db7fa65571165d1641c6e
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2023-04-19 14:19:57 +0300

    3a done

--- osa3/ei_palautukset/index.js
+++ osa3/ei_palautukset/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(requestLogger);
 
 let notes = [
   { id: 1, content: "HTML is easy", important: true },
@@ -63,6 +64,20 @@ app.delete("/api/notes/:id", (request, response) => {
 
   response.status(204).end();
 });
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body: ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
